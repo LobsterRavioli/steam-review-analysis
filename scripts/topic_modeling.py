ce592304f580b1183ed10c2136f65a4877819db0
@m.giella4 <m.giella4@studenti.unisa.it>
2024-12-04 12:00:37 +0100

    Refactor topic model: removed domain-specific term: 'game' and adjusted topic count from 5 to 3

--- scripts/topic_modeling.py
+++ scripts/topic_modeling.py
@@ -19,8 +19,9 @@ def generate_topics(model, feature_names, num_top_words,output_file):
             #Heaviest feature first
             for i in topic.argsort()[:-num_top_words - 1:-1]:
                 word = feature_names[i]  # Prendi il nome della parola
-                weight = topic[i]
-                writer.writerow({"topic_number": topic_idx, "word": word, "weight": weight})
+                if word != "game":
+                    weight = topic[i]
+                    writer.writerow({"topic_number": topic_idx, "word": word, "weight": weight})
 
 
 
@@ -75,12 +76,12 @@ if __name__ == "__main__":
     tfidf_matrix = vectorizer.fit_transform(cleaned_reviews)
 
     # LDA per topic modeling
-    n_components = 5
+    n_components = 3
     lda = LatentDirichletAllocation(n_components=n_components, random_state=42, verbose=1)
     for _ in tqdm(range(1), desc="Fitting LDA Model", unit=" iteration"):
         lda.fit(tfidf_matrix)
 
 
 
-    generate_topics(lda, vectorizer.get_feature_names_out(), 10,output_file)
+    generate_topics(lda, vectorizer.get_feature_names_out(), 11,output_file)
 
