2597d44c8e690bf9f3a7d9f97a2b6a7002a639ca
@m.giella4 <m.giella4@studenti.unisa.it>
2024-12-07 13:29:54 +0100

    Added lemmatization for more robust preprocessing

--- scripts/topic_modeling.py
+++ scripts/topic_modeling.py
@@ -1,6 +1,7 @@
 import csv
 
 import pandas as pd
+from nltk import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 from nltk.corpus import stopwords
@@ -33,16 +34,20 @@ if __name__ == "__main__":
 
     # Preprocessing
     nltk.download('stopwords')
+    nltk.download('wordnet')
     stop_words = set(stopwords.words('english'))
+    lemmatizer = WordNetLemmatizer()
 
-    # Load dataset
+
+    # Load dataset and drop null reviews
     print("Loading data...")
     reviews = df['review'].dropna()
 
-    # Data cleaning and tokenization
+
     print("Cleaning data...")
-    cleaned_reviews = [' '.join([word for word in review.lower().split() if word not in stop_words]) for review in
-                       reviews]
+    # Clean, remove stop words, and lemmatize
+    cleaned_reviews= [' '.join([lemmatizer.lemmatize(word) for word in review.lower().split() if word not in stop_words])
+                      for review in reviews]
 
     # Term frequency
     print("Calculating TF-IDF vectors...")
